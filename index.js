891d2e33af28588255639435ffd730489af9c211
edu290386 <eduardo.torres@outlook.com>
2023-05-21 22:17:45 -0500

    improve: mobile, render function & score function

--- index.js
+++ index.js
@@ -1,29 +1,73 @@
+const pi = "1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109";
 const inputDecimals = document.getElementById("inputDecimals");
 const result = document.getElementById("result");
 const score = document.getElementById("score");
 const fail = document.getElementById("fail");
-const pi = "1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109";
+const restart= document.getElementById("restart");
+const opportunity = document.getElementById("opportunity");
+const gain = document.getElementById("gain");
+
 let totalscore = 0;
 let position = 0;
-let opportunities = 0;
+let fails = 0;
+let opportunities = "";
+const icons = {0:"🍔", 1:"🍺",2:"🍬", 3:"🍆"}
+
+function renderStars(){
+    opportunities ="";
+    for (let i = 3 - fails; i > 0 ; i--) {    
+        opportunities += icons[3-i];
+    }  
+    opportunity.innerText = opportunities;  
+}
+renderStars();
+
+function renderPoints(){
+    gain.innerText = `+ ${100} points`;
+}    
+
+function renderNumber(){
+    if (fails == 3){
+    gain.innerText = `Next number: ${pi[position]}`;
+    }
+}
+
 inputDecimals.addEventListener("keyup", event =>{
     event.target.value = "";
     if(event.key == "Enter") return;
     if(isNaN(event.key)) return;
     let text = event.key;
+    renderStars();
     if(text == pi[position]){
-        result.innerText += text;
         result.style.color =  "#3D685F";
+        result.innerText += text;
         position ++;
         totalscore ++;
-        score.innerText = `Your Score is: ${totalscore} puntos`;
+        score.innerText = `Your score is: ${totalscore * 100} points`;
+        renderPoints();
     } else {
         result.style.color =  "#e83d4b";
-        opportunities ++;
-        fail.innerText = `Fails: ${opportunities}`;
-        if(opportunities==5) 
+        fails ++;
+        renderStars();
+        
+        if(fails===3) {
             inputDecimals.disabled = true;
+            restart.style.display = "block";
+            opportunity.innerText = icons[3];
+            renderNumber();
+            
+        }
     }
 })
 
-
+function restartGame(){
+    restart.style.display = "none";
+    inputDecimals.disabled = false;
+    opportunities = 0;
+    fails=0
+    result.innerText = "π : 3.";
+    renderStars();
+    score.innerText = "Your Score: 0";
+    position = 0;
+    totalscore = 0;
+}
